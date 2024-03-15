01d640bca4761345014458e4c5b0e62908114d8c
13137wiut <bibovb22@gmail.com>
2024-03-15 16:41:58 +0500

    DbContext

---
+++ WAD.00013137/Data/IssueTrackerContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using WAD._00013137.Models;
+
+namespace WAD._00013137.Data
+{
+	public class IssueTrackerContext : DbContext
+	{
+		public IssueTrackerContext(DbContextOptions<IssueTrackerContext> options)
+			: base(options) { }
+
+		public DbSet<Issue> Issues { get; set; }
+		public DbSet<User> Users { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Issue>()
+				.HasOne(i => i.User)
+				.WithMany(u => u.Issues)
+				.HasForeignKey(i => i.UserId);
+		}
+	}
+}
