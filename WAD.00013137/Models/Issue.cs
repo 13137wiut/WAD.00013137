c03dc2068ea08344454c5283965680f7526364fd
13137wiut <bibovb22@gmail.com>
2024-03-15 16:41:37 +0500

    Models

---
+++ WAD.00013137/Models/Issue.cs
@@ -0,0 +1,15 @@
+﻿namespace WAD._00013137.Models
+{
+	public class Issue
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public bool IsResolved { get; set; }
+
+		// Foreign key and navigation property
+		public int UserId { get; set; }
+		public User User { get; set; }
+	}
+}
---
+++ WAD.00013137/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace WAD._00013137.Models
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+
+		// Navigation property for the related issues
+		public ICollection<Issue> Issues { get; set; }
+	}
+}
