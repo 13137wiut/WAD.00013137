ea6f6f17345e274fc6dec74e290fbeed30630c85
13137wiut <bibovb22@gmail.com>
2024-03-15 17:24:10 +0500

    Finally controllers added

--- WAD.00013137/Controllers/IssuesController.cs
+++ WAD.00013137/Controllers/IssuesController.cs
@@ -1,6 +1,70 @@
-﻿namespace WAD._00013137.Controllers
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD._00013137.DAL.Interfaces;
+using WAD._00013137.Dtos;
+using WAD._00013137.DTOs;
+using WAD._00013137.Models;
+
+namespace WAD._00013137.Controllers
 {
-	public class IssuesController
+	[Route("api/[controller]")]
+	[ApiController]
+	public class IssuesController : ControllerBase
 	{
+		private readonly IIssueRepository _issueRepository;
+		private readonly IMapper _mapper;
+
+		public IssuesController(IIssueRepository issueRepository, IMapper mapper)
+		{
+			_issueRepository = issueRepository;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<IssueDto>>> GetIssues()
+		{
+			var issues = await _issueRepository.GetAllIssuesAsync();
+			var issueDtos = _mapper.Map<IEnumerable<IssueDto>>(issues);
+			return Ok(issueDtos);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<IssueDto>> GetIssue(int id)
+		{
+			var issue = await _issueRepository.GetIssueByIdAsync(id);
+			if (issue == null) return NotFound();
+			var issueDto = _mapper.Map<IssueDto>(issue);
+			return Ok(issueDto);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<IssueDto>> PostIssue(IssueDto issueDto)
+		{
+			var issue = _mapper.Map<Issue>(issueDto);
+			await _issueRepository.CreateIssueAsync(issue);
+			return Ok(issueDto);
+		}
+
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutIssue(int id, IssueDto issueDto)
+		{
+			if (id != issueDto.Id) return BadRequest();
+			var issue = await _issueRepository.GetIssueByIdAsync(id);
+			if (issue == null) return NotFound();
+
+			_mapper.Map(issueDto, issue);
+			await _issueRepository.UpdateIssueAsync(issue);
+			return Ok("The record with " + id + " has been updated!");
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteIssue(int id)
+		{
+			var issue = await _issueRepository.GetIssueByIdAsync(id);
+			if (issue == null) return NotFound();
+
+			await _issueRepository.DeleteIssueAsync(id);
+			return Ok("Record with " + id + " has been deleted!");
+		}
 	}
 }
--- WAD.00013137/Controllers/UsersController.cs
+++ WAD.00013137/Controllers/UsersController.cs
@@ -1,6 +1,69 @@
-﻿namespace WAD._00013137.Controllers
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD._00013137.DAL.Interfaces;
+using WAD._00013137.DTOs;
+using WAD._00013137.Models;
+
+namespace WAD._00013137.Controllers
 {
-	public class UsersController
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsersController : ControllerBase
 	{
+		private readonly IUserRepository _userRepository;
+		private readonly IMapper _mapper;
+
+		public UsersController(IUserRepository userRepository, IMapper mapper)
+		{
+			_userRepository = userRepository;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<UserDto>>> GetUsers()
+		{
+			var users = await _userRepository.GetAllUsersAsync();
+			var userDtos = _mapper.Map<IEnumerable<UserDto>>(users);
+			return Ok(userDtos);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<UserDto>> GetUser(int id)
+		{
+			var user = await _userRepository.GetUserByIdAsync(id);
+			if (user == null) return NotFound();
+			var userDto = _mapper.Map<UserDto>(user);
+			return Ok(userDto);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<UserDto>> PostUser(UserDto userDto)
+		{
+			var user = _mapper.Map<User>(userDto);
+			await _userRepository.CreateUserAsync(user);
+			return Ok(userDto);
+		}
+
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutUser(int id, UserDto userDto)
+		{
+			if (id != userDto.Id) return BadRequest();
+			var user = await _userRepository.GetUserByIdAsync(id);
+			if (user == null) return NotFound();
+
+			_mapper.Map(userDto, user);
+			await _userRepository.UpdateUserAsync(user);
+			return Ok("The record with " + id + " has been updated!");
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteUser(int id)
+		{
+			var user = await _userRepository.GetUserByIdAsync(id);
+			if (user == null) return NotFound();
+
+			await _userRepository.DeleteUserAsync(id);
+			return Ok("Record with " + id + " has been deleted!");
+		}
 	}
 }
